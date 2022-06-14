98d3c754baf584953592cb2402915516a5c13369
mandelatuks <manderat206@gmail.com>
2022-06-14 09:51:27 +0200

    Add profifel basic

--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -1,6 +1,13 @@
 function ProfilePage() {
   return (
-    <h1>Mission</h1>
+    <section className="profile-page">
+      <div>
+        <h1> My Missions</h1>
+      </div>
+      <div>
+        <h1>My Rockets</h1>
+      </div>
+    </section>
   );
 }
 
