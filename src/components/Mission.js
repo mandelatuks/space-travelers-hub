71bcf2a4fb6066896752ab41952a492f40d03f1e
v0pe <dampblew01@gmail.com>
2022-06-14 07:43:51 -0400

    restrcture the layout

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,20 +1,38 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Mission extends React.PureComponent {
-    render() {
-      const { missions } = this.props;
-      return (
-        <ul>
-          {missions.map((mission) => (
-            <li key={mission.mission_id}>
-                {mission.mission_name}
-                {mission.description}
-                </li>
-          ))}
-        </ul>
-      );
-    }
+  render() {
+    const { missions } = this.props;
+    return (
+      <section className="missiontable">
+        <table>
+          <thead>
+            <tr>
+              <th><h4>Mission</h4></th>
+              <th><h4>Description</h4></th>
+              <th><h4>Status</h4></th>
+              <th><h4>.</h4></th>
+            </tr>
+          </thead>
+          <tbody>
+            {missions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td><h4>{mission.mission_name}</h4></td>
+                <td><h4>{mission.description}</h4></td>
+                <td><h4>Not a Member</h4></td>
+                <td><button type="button" className="cancel-btn">Join Mission</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    );
   }
+}
+
+Mission.propTypes = {
+  missions: PropTypes.string.isRequired,
+};
 
 export default Mission;
--- src/components/pages/MissionPage.js
+++ src/components/pages/MissionPage.js
@@ -8,31 +8,34 @@ class MissionPage extends React.Component {
       missions: [
         {
           mission_id: 1,
-          mission_name: 'Tales on the moon light',
-          description: 'Victor',
+          mission_name: 'Telstar',
+          description: 'VictorLorem ipsum, dolor sit amet consectetur adipisicing elit. Est, dolorem cupiditate accusamus molestiae fugiat suscipit distinctio, rerum odit id sunt quas voluptatum iusto tenetur deserunt laudantium possimus non voluptatem enim in perspiciatis at expedita quibusdam ab quasi! Pariatur, a iste laborum quis beatae hic harum numquam reprehenderit totam. Architecto beatae dignissimos dolorum commodi aspernatur. Exercitationem voluptate expedita est beatae a.',
         },
         {
           mission_id: 2,
-          mission_name: 'Joy on the ride',
-          description: 'Damola',
+          mission_name: 'Iridium Next',
+          description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, dolorem cupiditate accusamus molestiae fugiat suscipit distinctio, rerum odit id sunt quas voluptatum iusto tenetur deserunt laudantium possimus non voluptatem enim in perspiciatis at expedita quibusdam ab quasi! Pariatur, a iste laborum quis beatae hic harum numquam reprehenderit totam. Architecto beatae dignissimos dolorum commodi aspernatur. Exercitationem voluptate expedita est beatae a.',
         },
         {
           mission_id: 3,
-          mission_name: 'Jesus is the way, the truth and the light',
+          mission_name: 'Thaicom',
+          description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, dolorem cupiditate accusamus molestiae fugiat suscipit distinctio, rerum odit id sunt quas voluptatum iusto tenetur deserunt laudantium possimus non voluptatem enim in perspiciatis at expedita quibusdam ab quasi! Pariatur, a iste laborum quis beatae hic harum numquam reprehenderit totam. Architecto beatae dignissimos dolorum commodi aspernatur. Exercitationem voluptate expedita est beatae a.',
+        },
+        {
+          mission_id: 4,
+          mission_name: 'Commercial Resupply',
           description: 'The HolyBible',
         },
-
       ],
     };
   }
 
   render() {
     const { missions } = this.state;
-  return (
-    <Mission missions={missions}/>
-  );
-}
-
+    return (
+      <Mission missions={missions} />
+    );
+  }
 }
 
 export default MissionPage;
