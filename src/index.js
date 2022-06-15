e8bdc30b6fdd40fd9b05fffed7c0104f05977eee
mandelatuks <manderat206@gmail.com>
2022-06-15 14:56:47 +0200

    Linters fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import store from './Redux/ConfigureStore';
+import store from './Redux/Configurestore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
