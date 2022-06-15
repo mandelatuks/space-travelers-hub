2a99bbb89ddbd689bebb1cb233e3551d8dcc6f6d
mandelatuks <manderat206@gmail.com>
2022-06-16 01:51:54 +0200

    Add logic booking

--- src/components/pages/RocketPage.js
+++ src/components/pages/RocketPage.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchRockets } from '../../Redux/Rocket/rockets';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Rocket from '../Rocket';
 
 const RocketPage = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchRockets());
-  }, [dispatch]);
 
   return rockets.map((rocket) => <Rocket key={rocket.id} rocket={rocket} />);
 };
