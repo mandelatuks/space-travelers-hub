7121a0113b7c3ad8ed43c216db0ed3b66e7e7531
mandelatuks <manderat206@gmail.com>
2022-06-16 20:33:31 +0200

    Changes from the reviewer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import RocketPage from './components/pages/RocketPage';
 import MissionPage from './components/pages/MissionPage';
 import ProfilePage from './components/pages/ProfilePage';
 import Navbar from './components/pages/Navbar';
+import { fetchRockets } from './Redux/Rocket/rockets';
+import { FetchMissions } from './Redux/Mission/Mission';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+    dispatch(FetchMissions());
+  }, [dispatch]);
+
   return (
     <main>
       <Navbar />
--- src/components/pages/MissionPage.js
+++ src/components/pages/MissionPage.js
@@ -1,17 +1,12 @@
 // eslint-disable-next-line react-hooks/exhaustive-deps
 
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import { FetchMissions, selectMission } from '../../Redux/Mission/Mission';
+import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
+import { selectMission } from '../../Redux/Mission/Mission';
 import Mission from '../Mission';
 
 const MissionPage = () => {
   const missions = useSelector(selectMission, shallowEqual);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(FetchMissions());
-  }, [dispatch]);
 
   return (
     <Mission missions={missions} />
