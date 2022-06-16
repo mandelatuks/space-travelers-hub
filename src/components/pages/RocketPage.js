695f184f90e03184da49e6208969a93c84105371
mandelatuks <manderat206@gmail.com>
2022-06-16 11:40:48 +0200

    Add dispatch to the rocketPage

--- src/components/pages/RocketPage.js
+++ src/components/pages/RocketPage.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Rocket from '../Rocket';
+import { fetchRockets } from '../../Redux/Rocket/rockets';
 
 const RocketPage = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
 
   return rockets.map((rocket) => <Rocket key={rocket.id} rocket={rocket} />);
 };
