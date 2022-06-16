44868d72abb87a421b97cd5cc83aac47e2e30839
v0pe <dampblew01@gmail.com>
2022-06-16 13:14:45 -0400

    add the profile page test

--- src/__test__/profilePage.test.js
+++ src/__test__/profilePage.test.js
@@ -2,15 +2,15 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from '../../../Redux/Configurestore';
-import Mission from '../MissionPage';
+import store from '../Redux/Configurestore';
+import Profile from '../components/pages/ProfilePage';
 
 test('test the MissionPage snapshot', () => {
   const missionRenderer = renderer.create(
     <Provider store={store}>
       <BrowserRouter>
 
-        <Mission />
+        <Profile />
       </BrowserRouter>
     </Provider>
     ,
